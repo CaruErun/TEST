7a9fe2c07b19c6bf90eaa22fa1f2807395cd4c19
CaruErun <alsebwlw11@gmail.com>
2022-05-18 18:09:58 +0900

    caru

---
+++ server-workspace2/JSP_Project/src/com/kh/common/PageInfo.java
@@ -0,0 +1,69 @@
+package com.kh.common;
+
+public class PageInfo {
+	int listCount;//현재 총 게시글 개수
+	int currentPage;//현재페이지(사용자 요청 페이지)
+	int pageLimit; //페이지 하단 페이징 페이지 처리 개수
+	int boardLimit; //한 페이지에서 보여지는 게시글 개수
+	int maxPage; //가장 마지막 페이지
+	int startPage; // 페이지 하단 보여질 페이징 시작수
+	int endPage; //페이지 하단에 보여질 페이징 수 
+	
+	public PageInfo() {
+		super();
+	}
+	public PageInfo(int listCount, int currentPage, int pageLimit, int boardLimit, int maxPage, int startPage,
+			int endPage) {
+		super();
+		this.listCount = listCount;
+		this.currentPage = currentPage;
+		this.pageLimit = pageLimit;
+		this.boardLimit = boardLimit;
+		this.maxPage = maxPage;
+		this.startPage = startPage;
+		this.endPage = endPage;
+	}
+	public int getListCount() {
+		return listCount;
+	}
+	public void setListCount(int listCount) {
+		this.listCount = listCount;
+	}
+	public int getCurrentPage() {
+		return currentPage;
+	}
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+	public int getPageLimit() {
+		return pageLimit;
+	}
+	public void setPageLimit(int pageLimit) {
+		this.pageLimit = pageLimit;
+	}
+	public int getBoardLimit() {
+		return boardLimit;
+	}
+	public void setBoardLimit(int boardLimit) {
+		this.boardLimit = boardLimit;
+	}
+	public int getMaxPage() {
+		return maxPage;
+	}
+	public void setMaxPage(int maxPage) {
+		this.maxPage = maxPage;
+	}
+	public int getStartPage() {
+		return startPage;
+	}
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+	public int getEndPage() {
+		return endPage;
+	}
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+	
+}
---
+++ server-workspace2/JSP_Project/src/com/kh/member/controller/MemberUpdateController.java
@@ -0,0 +1,71 @@
+package com.kh.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.kh.member.model.service.MemberService;
+import com.kh.member.model.vo.Member;
+
+/**
+ * Servlet implementation class MemberUpdateController
+ */
+@WebServlet("/update.me")
+public class MemberUpdateController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberUpdateController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String userId = request.getParameter("userId");
+		String userName = request.getParameter("userName");
+		String phone = request.getParameter("phone");
+		String email = request.getParameter("email");
+		String address = request.getParameter("address");
+		String[] interestArr = request.getParameterValues("interest");
+		String interest = "";
+		
+		if(interestArr != null) {
+			interest = String.join(",", interestArr);
+		}
+		
+		Member m = new Member(userId,userName,phone,email,address,interest);
+		
+		Member updateMem = new MemberService().updateMember(m);
+		
+		if(updateMem == null) {
+			request.setAttribute("errorMsg", "회원정보 수정에 실패하였습니다.");
+			request.getRequestDispatcher("views/common/errorPage.jsp").forward(request, response);
+		}else {
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", updateMem);//동일 키 값 사용시 덮어씌우기
+			session.setAttribute("alertMsg", "회원정보 수정 완료");
+									// JSP+ ~
+			response.sendRedirect(request.getContextPath()+"/myPage.me");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
